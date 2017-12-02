ea1f36aa7cf9cccae3cf877791f320ae9aaaa554
Wiktor Łazarski <wlazarski@interia.pl>
2017-12-02 12:17:57 +0100

    Scheme of readGame function

--- Penguins/game_reader.h
+++ Penguins/game_reader.h
@@ -4,22 +4,45 @@
 typedef struct GameReader GameReader;
 struct GameReader
 {
-    char* inputFilePath;
-    Game* (*readGame)();
+    Game* (*readGame)(char* inputFilePath);
     //TODO : Make structure for input Params and return them with this function temporary return void
     void (*readInputParams)(int, char**);
 };
 
 //reads game from input file function
-static Game* readGame()
+static Game* readGame(char* inputFilePath)
 {
-    //reading and initializing values of player and field
+    int numberOfPlayers = 2, availablePenguins = 1;
+    Dimension boardDim;
+    boardDim.row = 10;
+    boardDim.col = 10;
 
-    //temporary solution
-    Dimension* boardDim = createDimension(20000, 10010);
-    Game* retGame = createGame(3, *boardDim);
-    finalizeDimension(boardDim);
+    //1. Read first row from input file to the variables above
+
+    Game* retGame = createGame(numberOfPlayers, boardDim);
+    if(retGame == NULL)
+        goto ErrorHandler;
+
+
+     int* scores = (int*) calloc(numberOfPlayers, sizeof(int));
+
+    //2. Read scores for every player to scores table
+
+    int i;
+    for(i = 0; i < numberOfPlayers; i++)
+    {
+        initPlayer(scores[i], availablePenguins, retGame->getPlayer(i, retGame));
+    }
+
+    //3. Read and initialize every field
+    int currField = numberOfPlayers + 1; // +1 because we don't expect that value in input file so every validation function will return false
+
+
     return retGame;
+
+ErrorHandler :
+    perror("Error while reading game from input file");
+    exit(EXIT_FAILURE);
 }
 
 //reads input parameters    TODO : CHANGE RETURN VALUE
@@ -30,10 +53,9 @@ static void readInputParams(int argc, char** argv)
 }
 
 //constructor
-GameReader* createGameReader(char* path)
+GameReader* createGameReader()
 {
     GameReader* retv = (GameReader*) malloc(sizeof(GameReader));
-    retv->inputFilePath = path;
     retv->readGame = &readGame;
     retv->readInputParams = &readInputParams;
 
