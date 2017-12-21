c186843fa5868516d9c271c4aa8b649c932a45a9
Wiktor Łazarski <wlazarski@interia.pl>
2017-12-21 23:01:03 +0100

    Final version of interactive mode

--- source_code/main.c
+++ source_code/main.c
@@ -1,55 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "game_reader.h"
-#include "command_params.h"
 #include "algorithms.h"
-#include "computer.h"
 
-//******* AUTOMATED MODE MAIN FUN ******************************
 
 int main(int argc, char** argv)
 {
     GameReader reader;
     initGameReader(&reader);
-
-    CommandParams commandLineParams = reader.readInputParams(argc, argv);
-
-    Game* game = reader.readGame(commandLineParams.inputPath);
-
-    Computer AI;
-    initComputer(&AI);
-    /*
-        AI STARTS TO DO JOB !
-    */
-    switch(commandLineParams.phase)
-    {
-        case MOVEMENT :
-            AI.movePenguin(game, commandLineParams.player); break;
-        case PLACEMENT :
-            AI.placePenguin(game, commandLineParams.player); break;
-        default :
-            return -1;
-    }
-
-    GameWriter writer;
-    initGameWriter(&writer);
-    writer.write(game, commandLineParams.outputPath);
-
-    finalizeGame(game);
-
-    return 0;
-}
-
-//*****************************************************************
-
-
-//******* INTERACTIVE MODE MAIN FUN ******************************
-
-/*int main(int argc, char** argv)
-{
-    GameReader reader;
-    initGameReader(&reader);
-    reader.readInputParams(argc, argv);
 
     Game* game = reader.readGame("./resources/input.txt");
 
@@ -59,7 +17,7 @@ int main(int argc, char** argv)
     game->state = PLACEMENT;
     gamePhase.placement(game);
 
-    game->state = GAME;
+    game->state = MOVEMENT;
     gamePhase.movement(game);
 
     GameWriter writer;
@@ -69,6 +27,4 @@ int main(int argc, char** argv)
     finalizeGame(game);
 
     return 0;
-}*/
-
-//*****************************************************************
+}
